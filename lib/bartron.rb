f157634ffb7576318eb064642e7a3f9c73765bb2
Jason Dusek <jason.dusek@gmail.com>
2011-01-26 16:36:36 -0800

    Retrieves XML.

---
+++ lib/bartron.rb
@@ -0,0 +1,76 @@
+module BARTron
+extend self
+
+  class QBall
+    def initialize(api_key)
+      @api_key = api_key
+    end
+    def query(params)
+      q = BARTron.hash2get( params.merge('key' => @api_key) )
+      "http://api.bart.gov/api/sched.aspx#{q}"
+    end
+  end
+
+  module Queries
+  extend self
+    def recent_trips(from, to)
+      { 'cmd' => 'depart', 'b' => '4', 'a' => '4', 'l' => '1',
+        'orig' => from,
+        'dest' => to }
+    end
+  end
+
+  def hash2get(hash)
+    # Use URL escaping some day.
+    hash.inject("") do |acc, pair|
+      k, v = pair
+      acc.empty? ? "?#{k}=#{v}" : "#{acc}&#{k}=#{v}"
+    end
+  end
+
+  STATIONS = { '12TH' => '12th St. Oakland City Center',
+               '16TH' => '16th St. Mission (SF)',
+               '19TH' => '19th St. Oakland',
+               '24TH' => '24th St. Mission (SF)',
+               'ASHB' => 'Ashby (Berkeley)',
+               'BALB' => 'Balboa Park (SF)',
+               'BAYF' => 'Bay Fair (San Leandro)',
+               'CAST' => 'Castro Valley',
+               'CIVC' => 'Civic Center/UN Plaza (SF)',
+               'COLS' => 'Coliseum/Oakland Airport',
+               'COLM' => 'Colma',
+               'CONC' => 'Concord',
+               'DALY' => 'Daly City',
+               'DBRK' => 'Downtown Berkeley',
+               'DUBL' => 'Dublin/Pleasanton',
+               'DELN' => 'El Cerrito del Norte',
+               'PLZA' => 'El Cerrito Plaza',
+               'EMBR' => 'Embarcadero (SF)',
+               'FRMT' => 'Fremont',
+               'FTVL' => 'Fruitvale (Oakland)',
+               'GLEN' => 'Glen Park (SF)',
+               'HAYW' => 'Hayward',
+               'LAFY' => 'Lafayette',
+               'LAKE' => 'Lake Merritt (Oakland)',
+               'MCAR' => 'MacArthur (Oakland)',
+               'MLBR' => 'Millbrae',
+               'MONT' => 'Montgomery St. (SF)',
+               'NBRK' => 'North Berkeley',
+               'NCON' => 'North Concord/Martinez',
+               'ORIN' => 'Orinda',
+               'PITT' => 'Pittsburg/Bay Point',
+               'PHIL' => 'Pleasant Hill/Contra Costa Centre',
+               'POWL' => 'Powell St. (SF)',
+               'RICH' => 'Richmond',
+               'ROCK' => 'Rockridge (Oakland)',
+               'SBRN' => 'San Bruno',
+               'SFIA' => "San Francisco Int'l Airport",
+               'SANL' => 'San Leandro',
+               'SHAY' => 'South Hayward',
+               'SSAN' => 'South San Francisco',
+               'UCTY' => 'Union City',
+               'WCRK' => 'Walnut Creek',
+               'WDUB' => 'West Dublin/Pleasanton',
+               'WOAK' => 'West Oakland' } 
+
+end
--- main.rb
+++ main.rb
@@ -1,6 +1,16 @@
 #!/usr/bin/env ruby
+require 'net/http'
+require 'uri'
 require 'rubygems'
+
+require 'json'
 require 'sinatra'
+
+$LOAD_PATH << File.expand_path(File.dirname(__FILE__)) + '/lib'
+require 'bartron'
+
+
+##  User-facing matter.
 
 not_found do
   headers["Status"] = "301 Moved Permanently"
@@ -12,8 +22,27 @@ get '/' do
   erb :index
 end
 
+
+##  API -- raw text or XML, maybe docs.
+
+BART_API_KEY = 'JMDV-QYQA-YHZT-MVWA'
+HANDLE = BARTron::QBall.new(BART_API_KEY)
+
 get '/api' do
   content_type 'text/html', :charset => 'utf-8'
   erb :api
+end
+
+get '/api/:from/:to' do
+  # content_type 'application/json', :charset => 'utf-8'
+  from, to = params[:from], params[:to]
+  unless BARTron::STATIONS[from] and BARTron::STATIONS[to]
+    content_type 'application/json', :charset => 'utf-8'
+    halt 400, JSON.dump( 'error' => 'bad station' )
+  end
+  query = BARTron::Queries.recent_trips(from, to)
+  s = HANDLE.query(query)
+  content_type 'text/plain', :charset => 'utf-8'
+  Net::HTTP.get(URI.parse(s))
 end
 
